cca4811f2b31aa402586722b99e4373a850d9d1e
Neige <72865616+Neige7@users.noreply.github.com>
2023-06-19 10:59:04 +0800

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,138 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Banker',
+  tagline: 'MythicMobs掉落物伤害分配',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://neige7.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/Banker-Wiki/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'neige7', // Usually your GitHub org/user name.
+  projectName: 'Banker-Wiki', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans', 'en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          editUrl:
+            'https://github.com/Neige7/Banker-Wiki/blob/main'
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Banker',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.svg',
+        },
+        hideOnScroll: false,
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: '开始',
+          },
+          // 搜索框
+          {
+            type: 'search',
+            position: 'left',
+          },
+          {
+            href: 'https://github.com/Neige7/Banker',
+            label: 'GitHub',
+            position: 'right',
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+        ],
+      },
+      // 底部链接
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '文档',
+            items: [
+              {
+                label: '开始',
+                to: '/intro',
+              },
+            ],
+          },
+          {
+            title: '交流',
+            items: [
+              {
+                label: 'QQ群',
+                href: 'https://jq.qq.com/?_wv=1027&k=QKurhX6E',
+              },
+            ],
+          },
+          {
+            title: '插件发布',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Neige7/Banker',
+              },
+            ],
+          },
+        ],
+        // 底部版权信息
+        copyright: `Copyright © ${new Date().getFullYear()} Neige, All Rights Reserved.`,
+      },
+      // 深浅主题
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      // 颜色随系统切换
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+    }),
+};
+
+module.exports = config;
